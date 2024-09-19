262f5b09e3a9d322ad8b466c118a5a66538e407c
sxpersxnic <melvin@kampus.ch>
2024-09-19 11:02:36 +0200

    env

---
+++ app/blog/(overview)/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Posts</h1>
+
+      <div>
+        <p>Here the most recent posts will be displayed</p>
+      </div>
+
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 
-export default function Page() {
+export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+
+      <div>
+        <p>Here unauthenticated users will be redirected to.</p>
+      </div>
+
     </main>
   );
 }
