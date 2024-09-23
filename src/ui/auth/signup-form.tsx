a3d7354705c46be0c6e122c042a74eca8e8d1db7
Sxnic <102521739+sxpersxnic@users.noreply.github.com>
2024-09-23 12:16:51 +0200

    Update signup-form.tsx

--- src/ui/auth/signup-form.tsx
+++ src/ui/auth/signup-form.tsx
@@ -1,50 +1,6 @@
-import { useFormState, useFormStatus } from 'react-dom';
-
 export function SignUpForm() {
 
   return (
-    <form>
-      <div>
-        <label htmlFor="username">
-          Username
-        </label>
-        <input id="username" name="username" placeholder="Enter Username" required />
-      </div>
-      {state?.errors?.username && <p>{state.errors.username}</p>}
-      <div>
-        <label htmlFor="email">
-          Email
-        </label>
-        <input id="email" name="email" type="email" placeholder="Enter Email" required />
-      </div>
-      {state?.errors?.username && <p>{state.errors.username}</p>}
-      <div>
-        <label htmlFor="password">
-          Password
-        </label>
-        <input id="password" name="password" type="password" />
-      </div>
-      {state?.errors?.password && (
-        <div>
-          <p>Password must:</p>
-          <ul>
-            {state.errors.password.map((error) => (
-              <li key={error}>- {error}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <SubmitButton />
-    </form>
-  )
-}
-
-function SubmitButton() {
-  const { pending } = useFormStatus()
-
-  return (
-    <button disabled={pending} type="submit">
-      Sign Up
-    </button>
-  )
+    <H1>Sign up Form</H1>
+  );
 }
