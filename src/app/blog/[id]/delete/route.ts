d63754d03a12f91b953f4e06b5da95365bb78dfa
sxpersxnic <melvin@kampus.ch>
2024-10-04 12:54:46 +0200

    Rm Delete route

--- src/app/blog/[id]/delete/route.ts
+++
@@ -1,9 +0,0 @@
-import { deletePost } from "@/lib/actions";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-
-export async function DELETE({ params }: { params: { id: string }}): Promise<void> {
-  await deletePost(params.id);
-  revalidatePath('/blog');
-  redirect('/blog');
-}
